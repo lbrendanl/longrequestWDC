c13ef2ba7eefb40c0b3fed44c19e52ecf3ebdafc
lbrendanl <lee33@uw.edu>
2016-06-13 11:29:53 -0700

    added some http requests

--- longrequestWDC.js
+++ longrequestWDC.js
@@ -21,6 +21,17 @@
              table.appendRows([{ "data": 1}]);
              doneCallback();
          }, 120000);
+
+        var i;
+        var apiURL = 'http://jsonplaceholder.typicode.com/comments';
+
+        for (i=0;i<1000;i++) {
+            $.ajax(apiURL, {
+                method: 'GET'
+            }).then(function(data) {
+                console.log('fetched some data');
+            });
+        }
      };
 
      setupConnector = function() {
